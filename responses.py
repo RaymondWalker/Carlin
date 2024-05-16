334ac51b3b0d08035345fcdf987ef373c2df899d
RaymondWalker <61421600+RaymondWalker@users.noreply.github.com>
2024-05-16 10:15:37 -0400

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,18 @@
+
+def get_responses(user_input: str ) -> str: 
+    lowered: str = user_input.lower()
+    if lowered== '':
+        return 'Can you repeat that?'
+    elif 'hello' in lowered:
+        return 'hello!'
+    elif 'dice roll' in lowered:
+        return f'you rolled: {randint(1,6)}'
+    elif 'spring' in lowered:
+        return 'boing - - ribbit '
+    else:
+        if 'sleep' in lowered:
+            return 'Goodnight'
+        else:
+            return choice(['I don't know what to say,
+                            'Put together a comprehensible sentence please',
+                            'You smell funny'])
